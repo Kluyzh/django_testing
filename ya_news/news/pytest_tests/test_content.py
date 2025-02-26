14390b313b451d802048535377a106ab57f888d8
Ilya Kluyzhev <ilia.kluyzhev.2498@gmail.com>
2025-02-26 03:26:12 +0300

    Change comments ordering in test

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -30,7 +30,7 @@ def test_oder_of_comments(client, news_object):
     news = response.context['news']
     all_comments = news.comment_set.all()
     all_timestamps = [comment.created for comment in all_comments]
-    sorted_timestamps = sorted(all_timestamps, reverse=True)
+    sorted_timestamps = sorted(all_timestamps)
     assert all_timestamps == sorted_timestamps
 
 
